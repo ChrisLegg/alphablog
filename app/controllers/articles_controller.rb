e8a036f7d58fe213014a67d68821af36ec65f330
Chris Legg <chris@navondigital.com>
2016-11-14 16:52:32 +0000

    Add edit and index pages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,19 @@
 class ArticlesController < ApplicationController
 
+  def index
+    @articles = Article.all
+  end
+
   def show
     @article = Article.find(params[:id])
   end
 
   def new
     @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
   end
 
   def create
@@ -18,6 +26,15 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated"
+      redirect_to @article
+    else 
+      render 'edit'
+    end
+  end
 
 private
   def article_params
